b4e3f4b9a8a42f16b2c8372ab84d828a9cbe382f
gabozarate14 <gabozarate14@gmail.com>
2022-10-26 15:40:37 +0200

    exploitation zone added

--- Operations/Zones/trustedZone.py
+++ Operations/Zones/trustedZone.py
@@ -2,7 +2,11 @@
 import Zones.trustedZone_control as tz1
 import Zones.trustedZone_load as tz2
 import Zones.trustedZone_deduplication as tz3
-import Zones.trustedZone_outliers as tz4
+import Zones.trustedZone_setcorrectformats as tz4
+import Zones.trustedZone_outliers as tz5
+import Zones.trustedZone_fixsampling as tz6
+
+
 
 
 import warnings
@@ -23,6 +27,11 @@ def trustedZone():
 		tz2.trustedZone_load()
 		print('3.3 Deduplication')
 		tz3.trustedZone_deduplication()
-		print('3.4 Outliers treatment')
-		tz4.trustedZone_outliers()
+		print('3.4 Set correct formats')
+		tz4.trustedZone_setcorrectformats()
+		print('3.5 Outliers treatment')
+		tz5.trustedZone_outliers()
+		print('3.6 Fixing the sample')
+		print('Columns fixed:')
+		tz6.trustedZone_fixsampling()
 	
--- Operations/main.py
+++ Operations/main.py
@@ -2,33 +2,47 @@
 import Zones.persistentLanding as pl
 import Zones.formattedZone as fz
 import Zones.trustedZone as tz
- 
+import Zones.exploitationZone as ez
+import warnings
+
+def fxn():
+	warnings.warn("deprecated", DeprecationWarning)
 
 def printLine():
 	print('='*70)
 
 def main():
-	
-	printLine()
-	print('1. Landing Zone:')
-	printLine()
-	print('Persistent Landing Zone:')
-	printLine()
-	print('Copying files from Temporal Landing to Persistent Landing')
-	pl.persistentLanding()
-	print('Files copied')
-	printLine()
-	print('2. Formatted Zone:')
-	printLine()
-	print('Saving files from Persistent Landing in the Formatted Zone DB')
-	print('(Tablename) - (num. of rows)')
-	fz.formattedZone()
-	print('Files saved as tables')
-	printLine()
-	print('3. Trusted Zone:')
-	printLine()
-	print('Moving data from the Formatted Zone DB to the Trusted Zone DB')
-	tz.trustedZone()
+
+	with warnings.catch_warnings():
+		warnings.simplefilter("ignore")
+		fxn()
+		print('STARTING LOADING')
+		printLine()
+		print('1. Landing Zone:')
+		printLine()
+		print('Persistent Landing Zone:')
+		printLine()
+		print('Copying files from Temporal Landing to Persistent Landing')
+		pl.persistentLanding()
+		print('Files copied')
+		printLine()
+		print('2. Formatted Zone:')
+		printLine()
+		print('Saving files from Persistent Landing in the Formatted Zone DB')
+		print('(Tablename) - (num. of rows)')
+		fz.formattedZone()
+		print('Files saved as tables')
+		printLine()
+		print('3. Trusted Zone:')
+		printLine()
+		print('Moving data from the Formatted Zone DB to the Trusted Zone DB')
+		tz.trustedZone()
+		printLine()
+		print('4. Exploitation Zone:')
+		print('Generating views in the Exploitation Zone DB')
+		ez.exploitationZone()
+		printLine()
+		print('LOADING FINISHED')
 
 
 if __name__ == "__main__":
