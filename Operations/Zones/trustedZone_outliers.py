2b95615593f7d9e96c822854ab7ca2ee2e0fb16e
gabozarate14 <gabozarate14@gmail.com>
2022-10-26 17:00:27 +0200

    outliers correction

--- Operations/Zones/trustedZone_outliers.py
+++ Operations/Zones/trustedZone_outliers.py
@@ -39,19 +39,55 @@ def trustedZone_outliers():
   con_trusted = duckdb.connect(dbFile_trusted)
   cursor_trusted = con_trusted.cursor()
 
+  """### Detection and deletion of univariate outliers"""
+
+  #Gets the tablenames except the control tables
+  tablenames_trusted = set(cursor_trusted.execute("select tablename from pg_tables where tablename not in ('tables_loaded')").fetchdf()['tablename'])
+
+  print('-------------------------')
+  print('Univariate Outlier Detection Report')
+  print('-------------------------')
+
+  #It goes through the tables to detect outliers 
+  for tablename in tablenames_trusted:
+    df = cursor_trusted.execute('select * from '+tablename).fetchdf()
+    df_num = df.select_dtypes(include='number')
+    df_num = df_num.dropna(axis='columns', how='all')
+    df_post =df
+    
+    #For each numeric column it calculates the z-score and then it will filter z-scoes higher than 3
+    for col in df_num:
+        df['zcore']  = np.abs(stats.zscore(df_num[col]))
+        df_post.drop(df_post[df_post.zcore < 3].index, inplace=True)
+
+        #We delete the additional columns
+        df_post.drop(['zcore'], axis=1, inplace=True)
+
+    #Reviews if there are any outliers
+
+    #If there is outliers, it 'updates' the table deleting the outliers
+    if len(df) > len(df_post):
+      cursor_trusted.execute('drop table '+tablename)
+      cursor_trusted.execute('create table  '+tablename+ ' as select * from df_post')
+      print(tablename+': ' +str(len(df))+' -> '+ str(len(df_post)))
+    
+    #If there is not outliers it does not do anything
+    else:
+      print(tablename+': No outliers')
+
   """### Detection and deletion of multivariate outliers"""
 
   #Imports chi2 to get the p-value of each Mahalanobis distance
   from scipy.stats import chi2
 
   #Gets the tablenames except the control tables
-  tablenames_trusted = set(cursor_trusted.execute("select tablename from pg_tables where tablename not in ('tables_loaded')").fetchdf()['tablename'])
+  tablenames_trusted = set(cursor_trusted.execute("select tablename from pg_tables where tablename not in ('tables_loaded','observations')").fetchdf()['tablename'])
 
   #Define the numeric types for columns
   numerics = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64']
 
   print('-------------------------')
-  print('Outlier Detection Report')
+  print('Multivariate Outlier Detection Report')
   print('-------------------------')
 
   #It goes through the tables to detect outliers 
@@ -72,7 +108,7 @@ def trustedZone_outliers():
       #We delete the individuals with p-value less than 0.01
       df_post.drop(df_post[df_post.p < 0.01].index, inplace=True)
       
-      #We delete the aditional columnas
+      #We delete the adsitional columns
       df_post.drop(['mahalanobis', 'p'], axis=1, inplace=True)
 
     #Reviews if there are any outliers
